630c4e5dfbf9ab0ff2192b8882755260df1b0d97
lranch <44074178+lranch@users.noreply.github.com>
2024-12-11 13:54:50 -0600

    Add About page

---
+++ about.js
@@ -0,0 +1,27 @@
+/* TODO: import FontAwesome in the HTML file as a <script> tag */
+/* import "https://kit.fontawesome.com/f0e4070067.js"; */
+
+/* color palette:
+#082B59 bright blue
+#131D26 navy blue
+#59331D coffee
+#D98555 peach
+#F2F2F2 white
+
+Fonts:
+Headlines - Poppins Bold
+Text - Lato
+
+Links all go to https://uxuicurriculum.wixsite.com/gulproast
+
+<!--
+Dark logo: https://assets.codepen.io/6306176/gulp-logo-dark.png
+Light logo: https://assets.codepen.io/6306176/gulp-logo-light.png
+-->
+
+*/
+
+/* redirectOnClick provides on-click functionality for navigation */
+function redirectOnClick (e) {
+    document.location = "https://uxuicurriculum.wixsite.com/gulproast";
+  }
